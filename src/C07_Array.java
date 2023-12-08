4a331b4a0963d872e3227f11327dc9d7563ebe45
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2023-12-08 17:34:44 +0900

    2023.12.01 8th end

--- src/C07_Array.java
+++ src/C07_Array.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class C07_Array {
@@ -26,9 +27,89 @@ public class C07_Array {
 
 //        int [] int_arr4 = new int[]; 길이를 정해줘야함, 나중에 늘리는 것은 불가능.
 
+
+
+
 */
 
 
+
+/*
+        String[] arr_st = new String[5];
+        String[] arr_st1 = {"123","1234","1234","142"};
+
+        for(String a : arr_st1)
+            print(a);
+*/
+
+
+
+
+/*
+        int[] arr = {12, 3, 6, 56, 3, 2, 4, 5, 67, 2, 3, 4, 0};
+        Arrays.sort(arr);
+
+
+
+//        객체타입인 경우에만 Comparator클래서 객체 사용가능
+
+        print(Arrays.toString(arr));
+
+
+        String[] str = {"abc", "bc", "dg","aa","bdd"};
+        Arrays.sort(str);
+        print(Arrays.toString(str));
+
+
+        Arrays.sort(str, Comparator.reverseOrder());
+        print(Arrays.toString(str));
+
+
+        Integer[] arr_Integer = {12, 3, 6, 56, 3, 2, 4, 5, 67, 2, 3, 4, 0};
+        Arrays.sort(arr_Integer, Comparator.reverseOrder());
+        print(Arrays.toString(arr_Integer));
+
+        */
+
+
+
+//        방법 2 배열 뒤집기.
+/*
+
+        int[] arr1 = {4, 12, 3, 6, 56, 3, 2, 4, 5, 67, 2, 3, 4, 0};
+        Arrays.sort(arr1);
+        int temp = 0;
+        for ( int i = 0 ; i < arr1.length/2 ; i++){
+            temp = arr1[i];
+            arr1[i] = arr1[arr1.length - i - 1];
+            arr1[arr1.length - i - 1] = temp;
+        }
+
+        print(Arrays.toString(arr1));
+
+*/
+
+
+//        StreamApi, lamda를 활용한 내림 차순 정렬.
+
+
+        int[] arr2 = {9, 4, 12, 3, 6, 56, 3, 2, 4, 5, 67, 2, 3, 4, 0};
+
+        int[] new_arr2 = Arrays.stream(arr2)                    // arr2 대상으로 Stream 객체 생성
+                .boxed()                                        // Interger 형 변환한 스트림 객체 생성
+                .sorted(Comparator.reverseOrder())              // 내림차순 정렬
+                .mapToInt( a -> a )                             // Interger 를 인트로 변환 ( 람다 )
+                .toArray();                                     // 배열로 변환
+
+        print(Arrays.toString(new_arr2));
+
+
+
+
+
+
+
+//      실습
 /*
 
         int sum = 0;
@@ -84,7 +165,11 @@ public class C07_Array {
 */
 
 
-/*      for upload ********************
+//      배열 뒤집기.  for upload ********************
+
+
+
+/*
 
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
@@ -95,6 +180,29 @@ public class C07_Array {
             arr2[--length] = a;
 
         print(Arrays.toString(arr2));
+
+
+
+// 배열 뒤집기 2
+
+*/
+
+
+
+/*
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1};
+        int temp = 0;
+
+        int length = arr.length;
+
+        for ( int i = 0 ; i < length/2 ; i++ ){
+            temp = arr[i];
+            arr[i] = arr[ length - i - 1 ];
+            arr[ length - i - 1 ] = temp;
+        }
+
+        print(Arrays.toString(arr));
 
 */
 
