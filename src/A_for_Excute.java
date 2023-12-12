0014c021440303503628fa63aede42dd099f37c5
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2023-12-12 16:55:30 +0900

    2023.12.12 7th

--- src/A_for_Excute.java
+++ src/A_for_Excute.java
@@ -26,12 +26,57 @@ public class A_for_Excute {
         myMap.remove("baseball");
 
 
+        // key값 하나씩 출력.
         print(myMap.keySet()+"");
+        print(myMap.values()+"");
+
+        for(String a : myMap.keySet()) print("keuSet() 출력 : " + a);
+        for(String a : myMap.values()) print("value() 출력 : " + a);
+
+
+        // Iterator 이용한 방법
+        Iterator<String> myIter = myMap.keySet().iterator();
+        while(myIter.hasNext())
+            print("HasNext로 출력 : " + myIter.next());
+
+
+        List<String> myList = new ArrayList<>();
+        myList.add("basketball");
+        myList.add("basketball");
+        myList.add("basketball");
+        myList.add("kbaseball");
+        myList.add("kbaseball");
+        myList.add("kbaseball");
+        myList.add("kbaseball");
+        myList.add("tennis");
+        myList.add("tennis");
+        myList.add("tennis");
+        myList.add("tennis");
+        myList.add("kbaseball");
+        myList.add("kbaseball");
+        myList.add("tennis");
+        print("");
 
 
 
+        Map<String,Integer> myMap1 = new HashMap<>();
+        for(String S : myList) {
+            if (myMap1.containsKey(S))
+                myMap1.put(S, myMap1.get(S) + 1 );
+            else
+                myMap1.put(S,1);
+        }
 
 
+        // upload
+        Map<String,Integer> myMap2 = new HashMap<>();
+        for(String S : myList)
+            myMap2.put(S, myMap2.getOrDefault(S, 0)+1);
+
+
+
+        for(String S : myMap1.keySet())
+            print(S + "를 " + myMap1.get(S) +"명이 좋아합니다.");
 
 
 
