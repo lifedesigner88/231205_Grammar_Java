e2da36780ca69b5b18361da7cb92011618447e07
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2023-12-14 17:46:08 +0900

    2023.12.14 8th

--- src/ClassLecture/C12_03_Class2.java
+++ src/ClassLecture/C12_03_Class2.java
@@ -2,16 +2,32 @@ package ClassLecture;
 
 public class C12_03_Class2 {
     public static void main(String[] args) {
+        print("");
 
-        Person.person_total++;
         Person sejong = new Person();
 
-        sejong.name = "박세종";
-        sejong.age = 11;
+        sejong.setAge(21);
+        sejong.setAge(10);
+        sejong.setName("세종");
+        sejong.setName("세종ㅁㄴㅇㄻㄴㄹㄴㅁㅇㄻㄴㄹ");
+        sejong.setEmail("saintsj@naver.com");
+        sejong.setEmail("234.ㅁㄴㅇㄹ");
+
+        print("");
+        print(sejong.getName() + "의 정보는  "
+                + sejong.getAge() + "살  "
+                + sejong.getEmail());
 
 
-        print(sejong.whoIs());
+        print("");
+        Person jongkyn = new Person();
+        jongkyn.setName("종균");
+        jongkyn.setAge(34);
+        jongkyn.setEmail("lifedesinger@outlook.kr");
 
+        print(jongkyn.getName() + "의 정보는  "
+                + jongkyn.getAge() + "살  "
+                + jongkyn.getEmail());
     }
 
     private static void print(String a) {
@@ -24,23 +40,63 @@ public class C12_03_Class2 {
 
 
 
-
 class Person {
 
-    static int person_total;
+    private String name ;
+    private String email;
+    private String password;
 
-    String name ;
-    String email;
-    String password;
-    int age;
+    private int age;
 
-    String whoIs(){
-        return "내 이름은 " + name      + "입니다. "
-         + "이메일은 "      + email     + "이고, "
-         + "비밀번호는 "    + password   + "이며, "
-         + "나이는 "       + age        + "살 입니다.";
 
+//    String whoIs(){
+//        return "내 이름은 " + name      + "입니다. "
+//         + "이메일은 "      + email     + "이고, "
+//         + "비밀번호는 "    + password   + "이며, "
+//         + "나이는 "       + age        + "살 입니다.";
+//
+//    }
+
+
+    public String getName() {
+        return name;
     }
+
+
+    public void setName(String name) {
+        if (name.length() > 10)
+            print("이름이 너무 깁니다");
+        else
+            this.name = name;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (!email.contains("@"))
+            print("이메일 형식이 아닙니다");
+        else
+            this.email = email;
+    }
+
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 20)
+            print("나이가 너무 어립니다");
+        else
+            this.age = age;
+    }
+
+
 
     private void print(String a) {
         System.out.println(a);
